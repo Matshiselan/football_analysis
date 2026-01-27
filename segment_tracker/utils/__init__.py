27eebb1b33c8ca34105423eec074944a12f1bb26
Matshiselan <matshiselan@gmail.com>
2026-01-27 19:22:28 +0100

    sam2_init file

--- segment_tracker/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-# utils/__init__.py
-
-from .video_utils import read_video, save_video
-from .bbox_utils import get_foot_position
-
-# 🔑 EXPOSE THIS
-from segment_tracker.mask_utils import get_bbox_from_mask
