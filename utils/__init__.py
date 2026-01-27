bcfe01ba9b3f017f7cb905efd7ff4ebacac8fa39
Matshiselan <matshiselan@gmail.com>
2026-01-27 19:22:28 +0100

    sam2_init file

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,5 @@
 from .video_utils import read_video, save_video
 from .bbox_utils import get_center_of_bbox, get_bbox_width, measure_distance,measure_xy_distance,get_foot_position
+
+# 🔑 EXPOSE THIS
+from segment_tracker.mask_utils import get_bbox_from_mask
