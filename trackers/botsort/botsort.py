ab63c021a7eb3ec5608910f2d709b73fed1aca80
Matshiselan <matshiselan@gmail.com>
2026-01-30 15:45:36 +0100

    bort_sort

---
+++ trackers/botsort/botsort.py
@@ -0,0 +1,11 @@
+# BoT-SORT tracker integration stub
+# Download the official BoT-SORT code and place the main tracker class here.
+# For now, this is a placeholder. The real implementation will be added next.
+
+class BoTSORT:
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def update(self, detections):
+        # Should return list of tracks in the format: [ [bbox, id, ...], ... ]
+        return []
